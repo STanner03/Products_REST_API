ff1e37a961d26133a0dadcaf7af008cc63ce2b87
Shane Tanner <shanetanner03@gmail.com>
2022-10-07 09:51:58 -0500

    Created the fuctionality to Update a specific Product in the Products App Views file.

--- products_project/settings.py
+++ products_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'products',
 ]
@@ -48,7 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'products_project.urls'
 
