4c79bde73ef83478001617d76191f2973a71f8b1
Shane Tanner <shanetanner03@gmail.com>
2022-10-10 02:18:45 -0500

    Updated Reviews Views.py file + Confirmed fuctionality in Postman + Transferred Postman Collection, MySQL Screenshots, and drawSQL to project folder.

--- reviews/views.py
+++ reviews/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ReviewSerializer
 from .models import Review
-from products import *
 
 @api_view(['GET', 'POST'])
 def reviews_list(request):
@@ -22,9 +21,8 @@ def reviews_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def review_detail(request, pk):
     review = get_object_or_404(Review, pk=pk)
-    product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
-        serializer = ProductSerializer(product)
+        serializer = ReviewSerializer(review)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
         serializer = ReviewSerializer(review, data=request.data)
